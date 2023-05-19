3cc06cd3e91dbf812169172ffcb4331d766b57cd
jooooo92 <guswn8013@gmail.com>
2023-05-19 18:32:04 +0900

    23.05.19 추가

---
+++ src/Array_1/Bj_07.java
@@ -0,0 +1,15 @@
+package Array_1;
+
+public class Bj_07 {
+	public static void main(String[] args) {
+		
+		String str = "abcdefghijklmnopqrstuvwxyz";
+		String[] arr = new String[str.length()]; 
+		
+		
+		
+		
+		
+	}
+
+}
